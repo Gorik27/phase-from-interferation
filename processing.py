de2d1359600540c27c5d4de93a6cc50316dba3ea
Gorik27 <georgiy.marchiy@gmail.com>
2025-09-19 14:37:36 +0300

    first commit

---
+++ processing.py
@@ -0,0 +1,250 @@
+"""
+This file contains the "Process" function, which takes a signal with the 
+interference to the input, and returns a phase map, a deployed phase, a window 
+width containing +1 peaks in X and Y (the latter are needed if you need to make
+several measurements with the same window width)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft2 as fft
+from scipy.fft import ifft2 as ifft
+from scipy.fft import fftshift
+from sklearn.cluster import KMeans
+from skimage.restoration import unwrap_phase
+from scipy.signal import windows
+
+
+
+# The accuracy of the result greatly depends on the following parameters of processing and discharge +1 peak
+def process(x, y, Z, 
+            dx=False,
+            dy=False,
+            N_clusters=3,   # The number of peaks in Fourier (normal 3)
+            mask_coef=0.75, # The level of which is determined by peaks (all that is higher than mask_coef*maximum value)
+            window_coef=10, # How much the window width is greater than the peak width of the mask_coef level 
+            freq_window='hann'): # Choosing a filter window
+                                 # Options: 'no' - without a window
+                                 # 'hann' - Hann window
+                                 # 'bh' - blackmanharris (I dont prefer it)
+                                 # 'cos' - xosine window
+                                 # 'tukey' - tukey with parameter equal to 0.5 (flat in the middle)
+    window_coef_x = window_coef
+    window_coef_y = window_coef
+    X, Y = np.meshgrid(x, y)
+    n = len(x)
+    m = len(y)
+    lx = np.ptp(x)
+    ly = np.ptp(y)
+    """
+    Plot signal
+    """
+    plt.figure(dpi=500)
+    plt.subplot(321)
+    plt.gca().set_aspect('equal')
+    plt.contourf(x, y, Z)
+    plt.colorbar()
+    plt.title('Signal')
+    mask2 = (Z!=0)
+    
+    """
+    Fourier
+    """
+    
+    F = fftshift(fft(Z))
+    Fz = np.log(np.abs(F))
+    msk = (Fz>mask_coef*Fz.max())
+    plt.subplot(322)
+    plt.gca().set_aspect('equal')
+    plt.contourf(x, y, Fz)
+    plt.title('Fourier')
+    plt.colorbar()
+    
+    """
+    Find window consisting +1 peak
+    """
+    XY = np.column_stack((X[msk].ravel(), Y[msk].ravel()))
+    clusters = KMeans(n_clusters=N_clusters, n_init='auto').fit_predict(XY)
+    
+    plt.subplot(323)
+    plt.gca().set_aspect('equal')
+    plt.scatter(XY[:, 0], XY[:, 1], c=clusters, s=1)
+    plt.title('Selecting +1 peak')
+    flag = True
+    x1, x2 = 0, x.max()
+    y1, y2 = 0, y.max()
+    dx_m = 0.001*lx
+    dy_m = 0.001*ly
+    while flag and x1<x2 and y1<y2:
+        mask_x = (XY[:, 0] > x1)*(XY[:, 0] < x2)
+        mask_y = (XY[:, 1] > y1)*(XY[:, 1] < y2)
+        mask = mask_x*mask_y
+        if len(set(clusters[mask])) == 1:
+            flag = False
+        else:
+            x1+=dx_m
+            y1+=dy_m
+            
+    if x1==0 or y1==0:
+        x1, x2 = 0, x.max()
+        y1, y2 = y.min(), 0
+        flag = True
+        while flag and x1<x2 and y1<y2:
+            mask_x = (XY[:, 0] > x1)*(XY[:, 0] < x2)
+            mask_y = (XY[:, 1] > y1)*(XY[:, 1] < y2)
+            mask = mask_x*mask_y
+            if len(set(clusters[mask])) == 1:
+                flag = False
+            else:
+                x1+=dx_m
+                y2+=-dy_m
+                
+        if x1==0 or y2==0:
+            print('Error in detecting +1 peak!!!!!!!!!')
+            
+    c_s = list(set(clusters[mask]))
+    if len(c_s)>0:
+        c0 = c_s[0]
+    else:
+        c0 = 0
+        print('Peak +1 is not found!!!!!!\nThe random peak would be selected')
+    
+    mask = (clusters==c0)
+    xs = XY[:, 0][mask]
+    ys = XY[:, 1][mask]
+    
+    x1, x2 = xs.min(), xs.max()
+    y1, y2 = ys.min(), ys.max()
+    
+    #xm, ym = xs.mean(), ys.mean()
+    im = np.argmax((Fz[msk]).ravel()[mask])
+    xm, ym = xs[im], ys[im]
+    
+    if dx:
+        pass
+    else:
+        dx = (x2-x1)*window_coef_x
+    x1 = xm-dx/2
+    x2 = xm+dx/2
+    
+    if dy:
+        pass
+    else:
+        dy = (y2-y1)*window_coef_y
+    y1 = ym-dy/2
+    y2 = ym+dy/2
+    
+    # x1 = xm-(xm-x1)*window_coef_x
+    # y1 = ym-(ym-y1)*window_coef_y
+    # x2 = xm+(x2-xm)*window_coef_x
+    # y2 = ym+(y2-ym)*window_coef_y
+    
+    plt.plot([x1, x1, x2, x2, x1], [y1, y2, y2, y1, y1], color='black', alpha=0.3)
+    plt.xticks([])
+    plt.yticks([])
+    plt.plot([xm], [ym], 'x', color='red')
+    
+    """
+    Inverse Fourier and phase calculation
+    """
+    
+    Fzp = np.zeros((n, m))
+    Fp = np.zeros((n, m), dtype=np.complex128)
+    j1 = np.where(x>x1)[0][0]
+    j2 = np.where(x<x2)[0][-1]+1
+    i1 = np.where(y>y1)[0][0]
+    i2 = np.where(y<y2)[0][-1]+1
+    
+    dn = (i2-i1)
+    n0 = (n-dn)//2
+    dm = (j2-j1)
+    m0 = (m-dm)//2
+    
+    
+    def get_window(window_type, dn, dm, alpha=False):
+        if alpha:
+            wind_x = window_type(dm, alpha)
+            wind_y = window_type(dn, alpha)
+        else:
+            wind_x = window_type(dm)
+            wind_y = window_type(dn)
+        wind_2d = np.outer(wind_y, wind_x)
+        return wind_2d
+    
+    """
+    TODO: реализовать круглое окно w_2d(x, y) = w_1d(sqrt(x^2 + y^2))
+    """
+    # def get_window(window_type, dn, dm, alpha=False):
+    #     if alpha:
+    #         wind_x = window_type(dm, alpha)
+    #         wind_y = window_type(dn, alpha)
+    #     else:
+    #         wind_x = window_type(dm)
+    #         wind_y = window_type(dn)
+    #     wind_2d = np.outer(wind_y, wind_x)
+    #     return wind_2d
+        
+        
+    
+    match freq_window:
+        case 'hann':
+            print('Hann window is used')
+            window = get_window(windows.hann, dn, dm)
+        case 'bh':
+            print('Blackman-Harris window is used')
+            window = get_window(windows.blackmanharris, dn, dm)
+        case 'cos':
+            print('Cosine window is used')
+            window = get_window(windows.cosine, dn, dm)
+        case 'tukey':
+            print('Tukey window is used')
+            alpha = 0.5
+            window = get_window(windows.tukey, dn, dm, alpha)
+        case 'no':
+            window = np.ones((dn, dm))
+        case _:
+            print('Unkonwn window!!!\nNo window will be used!')
+            window = np.ones((dn, dm))
+    # plt.figure()
+    # plt.contourf(window)
+    # plt.show()
+    
+    
+    Fzp[n0:n0+dn, m0:m0+dm] = Fz[i1:i2, j1:j2]*window
+    Fp[n0:n0+dn, m0:m0+dm] = F[i1:i2, j1:j2]*window
+    
+    
+    plt.subplot(324)
+    plt.gca().set_aspect('equal')
+    plt.contourf(x[j1:j2], y[i1:i2], Fz[i1:i2, j1:j2]*window)
+    plt.xticks([])
+    plt.yticks([])
+    plt.colorbar()
+    plt.title('Centered and windowed +1 peak')
+    
+    Sp = ifft(Fp)
+    phi = np.arctan2(np.imag(Sp), np.real(Sp))
+    mask = np.ones_like(phi, dtype=bool)
+    mask[mask2] = False
+    phi = np.ma.array(phi, mask=mask)
+    
+    plt.subplot(325)
+    plt.gca().set_aspect('equal')
+    plt.contourf(x, y, phi/(np.pi))
+    plt.title('Phase [rad/$\pi$]')
+    plt.colorbar()
+    
+    
+    phi_u = unwrap_phase(phi)
+    
+    plt.subplot(326)
+    #np.mod(phi_u, 2*np.pi)
+    plt.contourf(x, y, phi_u/(np.pi))
+    plt.gca().set_aspect('equal')
+    plt.colorbar()
+    plt.title('Unwrapped phase [rad/$\pi$]')
+
+    
+    plt.gcf().tight_layout()
+    plt.show()
+    return phi, phi_u, dx, dy
---
+++ workflow.py
@@ -0,0 +1,122 @@
+"""
+An example of using the "Process" function for signal processing.
+
+The file generates the interference model signal, and then processes it to
+measure the phase difference between area covered with film and area uncovered
+"""
+from processing import process
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+remove_spikes = 1 #turn on stupid algorithm to remove spikes from final picture (sometimes may not work because it is stupid)
+use_equal_windows = 1 # make a window a second time equal to the first
+
+processing_settings = { #The accuracy of the result greatly depends on the following parameters of processing and discharge +1 peak
+'mask_coef' : 0.75,     # The level of which is determined by peaks (all that is higher than mask_coef*maximum value)
+'window_coef' : 10,     # How much the window width is greater than the peak width of the mask_coef level 
+'freq_window' : 'hann'  # Choosing a filter window
+                        # Options: 'no' - without a window
+                        # 'hann' - Hann window
+                        # 'bh' - blackmanharris (I dont prefer it)
+                        # 'cos' - xosine window
+                        # 'tukey' - tukey with parameter equal to 0.5 (flat in the middle)
+}
+
+N = 1000
+n, m = N, N
+lmb = 400*1e-9 #nm
+
+r1_i = 0.3 # Reflection from the film (center)
+r2_i = 0.6 # Reflection from the substrate (edge)
+drho_i = 1 # The difference in phases of amplitude coefficients of reflection of the substrate and film (layered system)
+
+l = 2*1e-2 #cm - The width of the screen
+R1 = 0.5 # radius of the film (center) in shares from L
+R2 = R1+0.4 # radius (thickness)
+
+R1*=l/2
+R2*=l/2
+
+theta = 1*1e-3 #mrads - angle between interfering rays
+psi = np.pi*(1/4+1/8) # The angle between the plum of the fall of light and the axis x
+
+P = lmb/2/theta 
+p = l/n
+
+print(f'Period (P): {P*1e2} cm')
+print(f'Pixel size (p): {p*1e2} cm')
+print(f'p < P/6? ----> {p<P/6}')
+
+
+
+r1 = r1_i
+r2 = r2_i
+k = 2*np.pi/lmb
+rho1 = drho_i
+rho2 = 0
+dz = 0#lmb*0.01
+
+
+x = np.linspace(-l/2, l/2, n)
+y = np.linspace(-l/2, l/2, m)
+
+X, Y = np.meshgrid(x, y)
+
+
+L = np.sin(theta)*(np.cos(psi)*X + np.sin(psi)*Y)
+Z = np.zeros((n, m))
+mask1 = (X*X+Y*Y<R1*R1)
+mask2 = (~mask1)*(X*X+Y*Y<R2*R2)
+
+Z[mask1] += 1+r1+2*np.sqrt(r1)*np.cos(rho1-k*(L[mask1]+2*dz))
+Z[mask2] += 1+r2+2*np.sqrt(r2)*np.cos(rho2-k*(L[mask2]+2*dz))
+
+phi1, phi1_u, dx, dy = process(x, y, Z, **processing_settings)
+
+r1 = r1_i
+r2 = r1_i
+rho1 = 0
+rho2 = 0
+dz = 0#lmb*0.01
+
+L = np.sin(theta)*(np.cos(psi)*X + np.sin(psi)*Y)
+Z = np.zeros((n, m))
+mask1 = (X*X+Y*Y<R1*R1)
+mask2 = (~mask1)*(X*X+Y*Y<R2*R2)
+
+Z[mask1] += 1+r1+2*np.sqrt(r1)*np.cos(rho1-k*(L[mask1]+2*dz))
+Z[mask2] += 1+r2+2*np.sqrt(r2)*np.cos(rho2-k*(L[mask2]+2*dz))
+
+if use_equal_windows:
+    phi0, phi0_u, _, _ = process(x, y, Z, dx=dx, dy=dy, **processing_settings)
+else:
+    phi0, phi0_u, _, _ = process(x, y, Z, **processing_settings)
+
+#%%
+#remove_skikes = True
+
+dphi_u = phi1_u-phi0_u
+dphi = phi1-phi0
+
+if remove_spikes:
+    va, vb = np.ma.median(dphi_u[mask1]), np.ma.median(dphi_u[mask2])
+    v1, v2 = min(va, vb)-1, max(va, vb)+1
+    pltmask = (dphi_u < v1) + (dphi_u > v2)
+    plt_dphi_u = np.ma.array(dphi_u, mask=pltmask)
+else:
+    plt_dphi_u = dphi_u
+
+
+plt.contourf(x, y, plt_dphi_u)
+plt.gca().set_aspect('equal')
+plt.colorbar()
+plt.title('Unwrapped phase  difference [rad/$\pi$]')
+
+dphi_m = np.ma.median(dphi_u[mask1])-np.ma.median(dphi_u[mask2])
+dphi_m *= -1
+print(f'Median phase difference: {dphi_m} (2pi modulo: {np.mod(dphi_m, 2*np.pi)})')
+plt.gcf().tight_layout()
+plt.show()
