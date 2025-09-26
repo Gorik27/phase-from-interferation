7e48687da49d33ae8a9dc0207743237d549e3756
Gorik27 <georgiy.marchiy@gmail.com>
2025-09-26 12:35:50 +0300

    remove separation of areas by mean

--- workflow_load.py
+++ workflow_load.py
@@ -11,11 +11,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+name = 'groove.txt'
+name_reference = 'groove_reference.txt'
+
+
 use_equal_windows = 1 # make a window a second time equal to the first
 
 processing_settings = { #The accuracy of the result greatly depends on the following parameters of processing and discharge +1 peak
 'mask_coef' : 0.75,     # The level of which is determined by peaks (all that is higher than mask_coef*maximum value)
-'window_coef' : 15,     # How much the window width is greater than the peak width of the mask_coef level 
+'window_coef' : 7.5,     # How much the window width is greater than the peak width of the mask_coef level 
 'freq_window' : 'hann'  # Choosing a filter window
                         # Options: 'no' - without a window
                         # 'hann' - Hann window
@@ -24,7 +28,7 @@ processing_settings = { #The accuracy of the result greatly depends on the follo
                         # 'tukey' - tukey with parameter equal to 0.5 (flat in the middle)
 }
 
-Z = np.loadtxt('data.txt')
+Z = np.loadtxt(name)
 mask = (Z!=0)
 noize_scale = 0.0
 Z[mask] *= np.random.normal(loc=1, scale=noize_scale, size=Z.shape)[mask]
@@ -39,7 +43,7 @@ X, Y = np.meshgrid(x, y)
 
 S1, phi1_u, dx, dy = process(x, y, Z, **processing_settings)
 
-Z0 = np.loadtxt('data_reference.txt')
+Z0 = np.loadtxt(name_reference)
 mask0 = (Z0!=0)
 Z0[mask0] *= np.random.normal(loc=1, scale=noize_scale, size=Z.shape)[mask]
 
@@ -49,24 +53,28 @@ else:
     S0, phi0_u, _, _ = process(x, y, Z0, **processing_settings)
 
 #%%
+S = (S0+S1)/2
+
 dphi_u = phi1_u-phi0_u
 dphi_u -= dphi_u.min()
 
-mn = dphi_u.mean()
-md = np.ma.median(dphi_u)
+mn = S.mean()
+md = np.ma.median(S)
+mask_md = (S<md)
 
-phi1_mn = np.ma.median(dphi_u[dphi_u<mn])
-phi2_mn = np.ma.median(dphi_u[dphi_u>=mn])
-dphi_mn = phi2_mn - phi1_mn
+xs = X[~mask_md].ravel()
+ys = Y[~mask_md].ravel()
 
-phi1_md = np.ma.median(dphi_u[dphi_u<md])
-phi2_md = np.ma.median(dphi_u[dphi_u>=md])
+
+phi1_md = np.ma.median(dphi_u[~mask_md])
+phi2_md = np.ma.median(dphi_u[mask_md])
 dphi_md = phi2_md - phi1_md
 
 remove_spikes = 1
 if remove_spikes:
-    va, vb = np.ma.median(dphi_u[dphi_u<mn]), np.ma.median(dphi_u[dphi_u>=mn])
-    v1, v2 = min(va, vb)-1, max(va, vb)+1
+    da = 10
+    va, vb = np.ma.median(dphi_u[mask_md]), np.ma.median(dphi_u[~mask_md])
+    v1, v2 = min(va, vb)-da, max(va, vb)+da
     pltmask = (dphi_u < v1) + (dphi_u > v2)
     plt_dphi_u = np.ma.array(dphi_u, mask=pltmask)
 else:
@@ -74,15 +82,23 @@ else:
 
 #plt_dphi_u = dphi_u
 
+plt.subplot(121)
 plt.contourf(x, y, plt_dphi_u)
 plt.gca().set_aspect('equal')
 plt.colorbar()
 plt.title('Unwrapped phase  difference [rad/$\pi$]')
 
+plt.subplot(122)
+plt.contourf(x, y, S)
+nn = 5
+plt.plot(xs[::nn], ys[::nn], '.', alpha=0.005, color='black')
+plt.gca().set_aspect('equal')
+plt.colorbar()
+plt.title('S2')
+
 
 dphi_ptp = dphi_u.ptp()
 print(f'Median phase difference (splitted by median): {dphi_md} (2pi modulo: {np.mod(dphi_md, 2*np.pi)})')
-print(f'Median phase difference (splitted by mean): {dphi_mn} (2pi modulo: {np.mod(dphi_mn, 2*np.pi)})')
 print(f'Peak to peak phase difference: {dphi_ptp} (2pi modulo: {np.mod(dphi_ptp, 2*np.pi)})')
 plt.gcf().tight_layout()
 plt.show()
